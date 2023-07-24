d7a87a85b5c9daa1d07e4815e4ab2f2f92a77a06
Shiva Gupta <shivagupta@sharus-Air.attlocal.net>
2023-07-24 15:35:03 -0400

    Cors Policy

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const productRoute = require("./routes/productRoute");
 const cors = require("cors");
-app.use(cors());
+
+var corsOptions = {
+  origin: "http://127.0.0.1:5173",
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/products", productRoute);
